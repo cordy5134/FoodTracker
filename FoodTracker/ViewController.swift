254b37fb24a4f58f3b405fc5e010b3d2725a4b91
Dongqi Yin <yin.475@osu.edu>
2020-01-27 10:50:37 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Dongqi Yin on 1/27/20.
+//  Copyright © 2020 Dongqi Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
